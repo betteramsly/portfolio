a03c5f89d8effc374fa28f0bc8a6dee763dcd077
Amin Beterbekow <betteramsly>
2022-08-13 17:35:44 +0300

    исправил верстку

--- js/main.js
+++ js/main.js
@@ -92,21 +92,6 @@ modalCloses.forEach((modalClose) => {
   })
 })
 
-/*==================== PORTFOLIO SWIPER  ====================*/
-let swiperPortfolio = new Swiper(".portfolio__container", {
-  cssMode: true,
-  loop: true,
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
 /*==================== TESTIMONIAL ====================*/
 var swiperTestimonial = new Swiper(".testimonial__container", {
   loop: true,
